7bb9148ee82aa916cc35d12b51c32b958e7d9dd3
KC <132471034+raid4lime@users.noreply.github.com>
2025-01-23 10:31:25 -0600

    2.0 PalinListRunner.java

--- PalinListRunner.java
+++ PalinListRunner.java
@@ -8,20 +8,18 @@ public class PalinListRunner
 	public static void main ( String[] args )
 	{
 		//add test cases
-		
 		String[] = testCases = {
-	   "one two three two one",
+		   "one two three two one",
            "1 2 3 4 5 one two three four five",
            "a b c d e f g x y z g f h",
            "racecar is racecar",
            "1 2 3 a b c c b a 3 2 1",
            "chicken is a chicken" 
-		
 		};
 		
 		 for(String test : testCases)
         {
-            SyntaxChecker checker = new SyntaxChecker(test); // makes an object
+            PalinList checker = new PalinList(test); // makes an object
             System.out.println(checker); // prints result
         }
 		
