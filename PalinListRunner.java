25626adb51dce6b67fae6cbc5cb494106567b46e
KC <132471034+raid4lime@users.noreply.github.com>
2025-01-24 11:12:40 -0600

    Fin PalinListRunner.java

--- PalinListRunner.java
+++ PalinListRunner.java
@@ -7,8 +7,8 @@ public class PalinListRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases
-		String[] = testCases = {
+		//adds test cases
+	   String[]testCases = {
 	   "one two three two one",
            "1 2 3 4 5 one two three four five",
            "a b c d e f g x y z g f h",
@@ -17,11 +17,10 @@ public class PalinListRunner
            "chicken is a chicken" 
 		};
 		
-		 for(String test : testCases)
-        {
-            PalinList checker = new PalinList(test); // makes an object
+	 for(String test : testCases)
+         {
+            PalinList checker = new PalinList(test); // makes a test object
             System.out.println(checker); // prints result
         }
-		
-	}
+    }
 }
